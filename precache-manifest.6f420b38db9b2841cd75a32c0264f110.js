4c4945b2d6301bbe4137415eedc9687c0fa494d0
mukhlisadhe <adhemukhlis@gmail.com>
2019-10-29 20:29:41 +0700

    init

---
+++ precache-manifest.6f420b38db9b2841cd75a32c0264f110.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b4fc5240677077e854a148e31511ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b0cfb396255302c16d3",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9952236a0e6f0152a1d7",
+    "url": "/static/js/2.2424f854.chunk.js"
+  },
+  {
+    "revision": "1b0cfb396255302c16d3",
+    "url": "/static/js/main.05557165.chunk.js"
+  },
+  {
+    "revision": "b25203b8836bf69600f3",
+    "url": "/static/js/runtime-main.ba9344fb.js"
+  },
+  {
+    "revision": "66165e234113cdec0cb77ab6da074d02",
+    "url": "/static/media/loginpage2.66165e23.svg"
+  }
+]);
